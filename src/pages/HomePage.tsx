e8fdea213d5cf0e37812273a98c32e1c78c98f9e
Wallace <wallacelim97@gmail.com>
2020-09-26 15:20:23 +0800

    Remove Button component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import {Button} from "../components/Button"
-import logo from '../logo.svg'
-
+import React from "react";
+import logo from "../logo.svg";
 
 export function HomePage() {
-    return <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>Welcome to JobDino</p>
-      <Button textContent="Start App" clickHandler={() => {}} />
-    </header>
-  </div>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Welcome to JobDino</p>
+      </header>
+    </div>
+  );
 }
